7b04c21f8c974d26ba3ca80f084dab5bafeb41aa
vivacityinfotech.jaipur <vivacityinfotech.jaipur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-31 15:13:07 +0000

    Adding Plugin Files To SVN
    git-svn-id: https://plugins.svn.wordpress.org/wp-google-analytics-scripts/trunk@885127 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,74 @@
+=== WP Google Analytics Sctipts ===
+Contributors: vivacityinfotech.jaipur
+Tags: 1.0
+Requires at least: 3.0
+Tested up to: 3.8
+License: GPLv2 or later
+
+WP Google Analytics Scripts - A simple plugin which lets you analyze your website traffic. 
+
+== Description ==
+
+WP Google Analytics Scripts provides statistics and basic analytical informations about your website. It helps one to estimate how traffic to a website changes after the launch of a new advertising campaign.
+
+It's provides information about the number of visitors to a website and the number of page views. It helps gauge traffic and popularity trends which is useful for market research.
+
+= Features =
+
+    * Easy install
+    * Very easy to configure.
+    * The Search Engine Optimization (SEO) reports in Google Analytics help you understand how Google Web Search queries led visitors to your site.
+    * Real-Time Reporting.
+    * User Permissions.
+    * Site-Speed analysis.
+    * In-Page Analytics.
+    * Advertising Reports.
+    * Easily Integrated with AdWords.
+    * It also provides more advanced features, including custom visitor segmentation.
+    
+
+= Rate Us / Feedback =
+
+Please take the time to let us and others know about your experiences by leaving a review, so that we can improve the plugin for you and other users.
+
+= Want More? =
+
+If You Want more functionality or some modifications, just drop us a line what you want and We will try to add or modify the plugin functions.
+
+== Available Languages ==
+
+    English
+
+== Installation ==
+
+Installation consists of following steps:
+
+1. Upload "WP Google Analytics Scripts" to the /wp-content/plugins/ directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place the "WP Google Analytics Scripts" Menu on your dashboard.
+
+== Frequently Asked Questions ==
+
+= How to create your Analytics UA code =
+
+1. To get your analytics UA code you need to login into your Google Analytics control panel.
+
+2. Into the topbar of the page click the Admin link.
+
+3. Click the Tracking Info link.
+
+4. Copy and paste the Tracking ID number into the "Google Analytics Scripts UA Tracking ID" under the Menu "WP Google Analytics Scripts" in your WordPress admin panel.
+
+5. A tracking ID number looks like this UA-2986XXXX-X.
+
+6. Copy and paste the Google Analytics Scripts into the "Google Analytics Footer Scripts" under the Menu "WP Google Analytics Scripts" in your WordPress admin panel. 
+
+
+== Screenshots ==
+1. WP Google Analytics Scripts plugin appears after activation in Plugin section  .
+2. WP Google Analytics Scripts settings page in the admin backend .
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ wp-google-analytics-scripts.php
@@ -0,0 +1,148 @@
+<?php
+/*
+Plugin Name: WP Google Analytics Scripts
+Plugin URI: http://vivacityinfotech.com/
+Description: WP Google Analytics Scripts generates detailed statistics about a website's traffic and traffic sources and measures conversions and sales.
+Author: VivacityInfotech
+Version: 1.0
+Author URI: http://vivacityinfotech.com/
+Requires at least: 3.8
+Text Domain: wp-google-analytics-scripts
+License: vivacityinfotech
+*/
+function Analytics_settings_page( $links ) {
+	$settings_block = '<a href="' . admin_url('admin.php?page=wp-google-analytics-scripts.php' ) .'">Settings</a>';
+	array_unshift( $links, $settings_block);
+	return $links;
+}
+
+$plugin = plugin_basename(__FILE__);
+add_filter( "plugin_action_links_$plugin", 'Analytics_settings_page' );
+
+function Analytics_uninstall() {
+	unregister_setting( 'Analytics_settings_page', 'Analytics_setting' );
+}
+
+register_uninstall_hook( __FILE__, 'Analytics_uninstall' );
+
+function Analytics_footer() {
+	ob_start();
+		$options = get_option( 'Analytics_setting' );
+		$sfhs_footer = isset( $options['footer_scripts_input'] ) ? $options['footer_scripts_input'] : '';
+		echo "<script type=text/javascript>\n";
+		echo $sfhs_footer;
+		echo "\n</script>";
+	echo ob_get_clean();
+}
+
+function Analytics_render() {
+	$options = get_option( 'Analytics_setting' );
+
+	if ( isset( $options['footer_scripts_input'] ) )
+		add_action( 'wp_footer', 'Analytics_footer' );
+}
+
+add_action( 'init', 'Analytics_render' );
+
+function Analytics_page_register() {
+	add_menu_page( 'VIVA Plugins', 'VIVA Plugins', 'manage_options', 'viva_plugins', 'Analytics_rendepage_submenu', '', 1001 );
+	add_submenu_page( 'viva_plugins','Add Custom Scripts','WP Google Analytics Scripts', 'manage_options', "fb_box_settings", 'Analytics_rendepage_submenu' );
+}
+
+add_action( 'admin_menu', 'Analytics_page_register' );
+
+function Analytics_settings_register() {
+	register_setting( 'Analytics_settings_page', 'Analytics_setting' );
+
+	add_settings_section( 'Analytics_section', __( '', 'rt_polls' ), 'Analytics_block', __FILE__ );
+
+	add_settings_field( 'Analytics_inputbox', __( 'Google Analytics Footer Scripts', 'rt_polls' ), 'Analytics_inputbox', __FILE__, 'Analytics_section' );
+
+	add_settings_field( 'Analytics_footerbox_track', __( 'Google Analytics UA Tracking ID', 'rt_polls' ), 'Analytics_footerbox_track', __FILE__, 'Analytics_section' );
+}
+
+add_action('admin_init', 'Analytics_settings_register');
+
+function Analytics_block() {} 
+
+
+function Analytics_inputbox() {
+	
+	$options  = get_option('Analytics_setting');
+	$field_value   = isset( $options['footer_scripts_input'] ) ? $options['footer_scripts_input'] : ''; ?>
+	
+	<textarea id="footer-scripts-input" name="Analytics_setting[footer_scripts_input]" placeholder=" Analytics Footer Scripts" style="width:300px; height: 200px;" ><?php echo esc_html( $field_value ) ?></textarea>
+	<p class="description"><?php echo 'Enter your Google Analytic Script .';?></p>
+	<?php
+}
+function Analytics_footerbox_track() {
+	$options  = get_option('Analytics_setting');
+	
+	$field_value   = isset( $options['footer_trackid_input'] ) ? $options['footer_trackid_input'] : ''; ?>
+	<input id="footer-trackid-input" name="Analytics_setting[footer_trackid_input]" placeholder="UA-2986XXXX-X." style="width:300px; " value="<?php echo esc_html( $field_value ) ?>"/>
+	<p class="description"><?php echo 'Enter your Google UA Code/ID here.';?></p>
+	<?php
+}
+add_action('admin_init','enqueue_styles');
+function enqueue_styles()
+{
+	wp_enqueue_style('style_plugin',plugins_url( 'css/plugin_style.css' , __FILE__ ) );	
+	
+	}
+function Analytics_rendepage_submenu() {
+	if ( isset( $_GET['settings-updated'] ) ) : ?>
+		<div id="message" class="updated"><p><?php _e( 'Scripts updated successfully.' ); ?></p></div>
+	<?php endif; ?>
+	<div class="wrap_main">
+	<div class="postbox plugin_wrap left">
+	<h3 class="hndle plugin_head"><?php echo 'Enter your Google Analytic Settings';?></h3>
+	<div class="inside">
+		<?php screen_icon(); ?>
+		<h2><?php 'Add Custom Scripts Plugin'; ?></h2>
+		<p><?php 'Add your own scripts (including Google Analytics) to your header or footer regardless of what theme you are using.' ?></p>
+		<form name="myform" action="options.php" method="post" enctype="multipart/form-data">
+			<?php settings_fields('Analytics_settings_page'); ?>
+			<?php do_settings_sections( __FILE__ ); ?>
+			<p class="submit">
+				<input name="scripts-submit" type="submit" class="button-primary" value='Update Scripts' />
+			</p>
+		</form>
+	</div>
+	</div>
+	<div class="postbox right ads_bar">
+			<h3 class="hndle" style="padding:10px;"><span>Follow Us</span></h3>
+			<div class="inside">
+			Please take the time to let us and others know about your experiences by leaving a review, so that we can improve the plugin for you and other users.
+<br>
+<h4>Want More?</h4>
+If You Want more functionality or some modifications, just drop us a line what you want and We will try to add or modify the plugin functions.
+			
+			</div>
+			</div>
+			</div>
+	<?php
+}
+add_action('wp_footer','viva_ua_code');
+function viva_ua_code() {
+
+	$ua_code = get_option( 'Analytics_setting' );
+	$ua_id = $ua_code['footer_trackid_input'];
+	$home_url = get_home_url();
+	$find = array( 'http://', 'https://', 'www.');
+	$replace = '';
+	$output = str_replace( $find, $replace, $home_url );
+
+	if($ua_id !== '') {
+		echo "
+		<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', '".$ua_id."', '".$output."');
+	ga('send', 'pageview');
+	</script>";
+	}
+
+}
